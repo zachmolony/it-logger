f51e9cd9ed4eb0ebc7badccda7eed96a8bfba8a9
zachmolony <zachary.molony@ada.ac.uk>
2020-09-09 18:46:25 +0100

    create log comps

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from 'react'
 import M from 'materialize-css'
 import 'materialize-css/dist/css/materialize.min.css'
 import SearchBar from './components/layout/SearchBar'
+import Logs from './components/logs/Logs'
 
 const App = () => {
   useEffect(() => {
     M.AutoInit()
   })
   return (
-    <div className="App">
+    <>
       <SearchBar />
-    </div>
-  );
+      <div className="container">
+        <Logs />
+      </div>
+    </>
+  )
 }
 
 export default App;
--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -3,12 +3,12 @@ import React from 'react'
 const SearchBar = () => {
     return (
       <nav className='mb-6 blue'>
-        <div class="nav-wrapper">
+        <div className="nav-wrapper">
           <form>
-            <div class="input-field">
+            <div className="input-field">
               <input id="search" type="search" required />
-              <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-              <i class="material-icons">close</i>
+              <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
+              <i className="material-icons">close</i>
             </div>
           </form>
         </div>
